7f2a260547081bce15238c90560b83b13b174316
Manurbhav Arya <cs21b057@iittp.ac.in>
2023-08-17 23:07:33 +0530

    navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,25 @@
 import 'package:flutter/material.dart';
 import './customWidgets/YearRange.dart';
-import './customWidgets/navbar.dart';
 import './customWidgets/selectArtForm.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import './customWidgets/HomePage.dart';
+import './customWidgets/ProfilePage.dart';
+import './customWidgets/MapPage.dart';
+import './customWidgets/StoriesPage.dart';
+import './customWidgets/navbar.dart';
+
 
 void main() async{
   /*await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );*/
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Title(
-          color: Colors.blueAccent,
-          child: const Center(
-            child: Text(
-              "Anvadhi",
-              style: TextStyle(
-                fontSize: 40.0,
-              ),
-            ),
-          ),
-        ),
-      ),
-      body: const Column(
-        children: [
-          SelectArtForm(),
-          YearRange()
-        ],
-      ),
-      bottomNavigationBar: const Navbar(),
-    ),
+    home: Navbar(),
   ));
 }
+
+
+
 
 
