728447666c923d8ca6d9017fb3e981c0db153a77
Chandradithya <chandradithya@gmail.com>
2023-08-18 20:40:18 +0530

    resolved conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async{
-  /*await Firebase.initializeApp(
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );*/
+  );
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
