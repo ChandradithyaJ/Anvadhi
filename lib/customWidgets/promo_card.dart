65347b1966eae3e4e8ce07e028e89e15caa360ce
Arpit Gupta <98796025+arpitguptagithub@users.noreply.github.com>
2023-08-20 03:38:53 +0530

    changhed promocard
    ssss

--- lib/customWidgets/promo_card.dart
+++ lib/customWidgets/promo_card.dart
@@ -1,45 +1,48 @@
 import 'package:flutter/material.dart';
 
 Widget promoCard(image, text, progress_percent) {
-  return Container(
-    clipBehavior: Clip.hardEdge,
-    margin: EdgeInsets.only(right: 15.0),
-    decoration: BoxDecoration(
-      color: Colors.orange,
-      borderRadius: BorderRadius.circular(10),
-      image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
-                0.1,
-                0.9
-              ], colors: [
-                Colors.black.withOpacity(.8),
-                Colors.black.withOpacity(.1)
-              ]),
+  return AspectRatio(
+    aspectRatio: 2.62 / 3,
+    child: Container(
+      clipBehavior: Clip.hardEdge,
+      margin: EdgeInsets.only(right: 15.0),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(10),
+        image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+                  0.1,
+                  0.9
+                ], colors: [
+                  Colors.black.withOpacity(.8),
+                  Colors.black.withOpacity(.1)
+                ]),
+              ),
+              child: Text(
+                text,
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
             ),
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
+            SizedBox(
+              height: 10,
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            child: LinearProgressIndicator(
-              value: progress_percent,
-              backgroundColor: Colors.black,
+            Container(
+              child: LinearProgressIndicator(
+                value: progress_percent,
+                backgroundColor: Colors.black,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
