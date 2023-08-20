0226a4039b9b899df7c7a975e71c2e549beaae38
Manurbhav Arya <110805836+mannubhai1@users.noreply.github.com>
2023-08-21 01:03:49 +0530

    Update Home_screen_culturia.dart
    cleaned the url for image fetch

--- lib/Home_screen_culturia.dart
+++ lib/Home_screen_culturia.dart
@@ -1,16 +1,49 @@
+import 'package:anvadhi/fireStoreImages.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:anvadhi/customWidgets/promo_card.dart';
 import 'package:anvadhi/customWidgets/promo_card_basic.dart';
 import 'package:anvadhi/Arts_display.dart';
 
+//firebase stuff
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
 
-class Home_screen_culturia extends StatelessWidget {
+class FireStorageService {
+  FireStorageService._();
+
+  static Future<String> loadFromStorage(String image) async {
+    return await FirebaseStorage.instance.ref().child(image).getDownloadURL();
+  }
+}
+
+class Home_screen_culturia extends StatefulWidget {
+  @override
+  State<Home_screen_culturia> createState() => _Home_screen_culturiaState();
+}
+
+class _Home_screen_culturiaState extends State<Home_screen_culturia> {
+  final storage = FirebaseStorage.instance;
+
+  Future<Widget> _getImage(BuildContext context, String image) async {
+    final ref = storage.ref().child(image);
+    final downloadUrl = await ref.getDownloadURL();
+    print(downloadUrl);
+    return Image.network(
+      downloadUrl,
+      fit: BoxFit.scaleDown,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.purple[50],
       body: SafeArea(
@@ -29,7 +62,7 @@ class Home_screen_culturia extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     Center(
-                      child: const Text(
+                      child: Text(
                         "Welcome to World of Cultures!",
                         style: TextStyle(color: Colors.pink, fontSize: 25),
                       ),
@@ -96,13 +129,53 @@ class Home_screen_culturia extends StatelessWidget {
                                 return Row(
                                   children: <Widget>[
                                     Expanded(
-                                        child: promoCardBasic(
-                                            'lib/assets/images/image.jpg',
-                                            "kalamkari",
-                                            0.1)),
-                                    // need to work to make promocard popup
+                                      //     child: promoCardBasic(
+                                      //         'lib/assets/images/image.jpg',
+                                      //         "kalamkari",
+                                      //         0.1)
+                                      child: FutureBuilder(
+                                        future: _getImage(context,
+                                            'Thang-Ta1.jpg'),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.done) {
+                                            // if (snapshot.hasError) {
+                                            //   return SomethingWentWrong();
+                                            // }
+                                            return Container(
+                                              height: MediaQuery.of(context)
+                                                      .size
+                                                      .height /
+                                                  1.25,
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width /
+                                                  1.25,
+                                              child: snapshot.data,
+                                            );
+                                          } else if (snapshot.connectionState ==
+                                              ConnectionState.waiting) {
+                                            return Container(
+                                              height: MediaQuery.of(context)
+                                                      .size
+                                                      .height /
+                                                  1.25,
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width /
+                                                  1.25,
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            );
+                                          } else {
+                                            // CircularProgressIndicator();
+                                            return Container();
+                                          }
+                                        },
+                                      ),
+                                    ),
                                   ],
-                                ); //need to shift children widget here
+                                );
                               },
                             ),
                           ],
@@ -141,11 +214,8 @@ class Home_screen_culturia extends StatelessWidget {
                         child: ListView(
                           scrollDirection: Axis.horizontal,
                           children: <Widget>[
-                            promoCard(
-                                            'lib/assets/images/image.jpg',
-                                            "kalamkari",
-                                            0.1),
-                           
+                            promoCard('lib/assets/images/image.jpg',
+                                "kalamkari", 0.1),
                           ],
                         ),
                       ),
@@ -153,8 +223,8 @@ class Home_screen_culturia extends StatelessWidget {
                         height: 20,
                       ),
                       Container(
-                        child: Center(
-                          // child: ElevatedButton(
+                        child: const Center(
+                            // child: ElevatedButton(
                             // child: Text('Continue  Button'),
                             // style: ElevatedButton.styleFrom(
                             //   primary: Colors.grey,
@@ -172,8 +242,8 @@ class Home_screen_culturia extends StatelessWidget {
                             //     MaterialPageRoute(
                             //         builder: (context) =>
                             //             Home_screen_culturia())),
-                          // ),
-                        ),
+                            // ),
+                            ),
                       ),
                     ],
                   )),
