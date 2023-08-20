3ad50ea63d93bd77872376730e1776625dc356b7
Chandradithya <chandradithya@gmail.com>
2023-08-20 11:44:33 +0530

    made changes

--- lib/customWidgets/MapPage.dart
+++ lib/customWidgets/MapPage.dart
@@ -7,14 +7,13 @@ class MapPage extends StatefulWidget {
   Map<String, dynamic> selectedArtForm;
   List<Map<String, dynamic>> ArtForms;
 
-  MapPage({ required this.ArtForms, required this.selectedArtForm }) : super();
+  MapPage({required this.ArtForms, required this.selectedArtForm}) : super();
 
   @override
   State<MapPage> createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-
   final TextEditingController artController = TextEditingController();
 
   // configure year range
@@ -24,51 +23,45 @@ class _MapPageState extends State<MapPage> {
   bool changedDropdown = true;
 
   @override
-  @override
   Widget build(BuildContext context) {
-
     // sort all art forms lexicographically
     widget.ArtForms.sort((a, b) => a['artName'].compareTo(b['artName']));
 
     // filter art forms in the time range
     List<Map<String, dynamic>> currentArtForms = [];
-    for(dynamic af in widget.ArtForms){
-      if(af['year'] >= yearRange.start && af['year'] <= yearRange.end){
+    for (dynamic af in widget.ArtForms) {
+      if (af['year'] >= yearRange.start && af['year'] <= yearRange.end) {
         currentArtForms.add(af);
       }
     }
 
-    if(currentArtForms.isNotEmpty && !changedDropdown){
-        widget.selectedArtForm = currentArtForms[0];
+    if (currentArtForms.isNotEmpty && !changedDropdown) {
+      widget.selectedArtForm = currentArtForms[0];
     }
 
     /** Error Handling **/
-    if(currentArtForms.isEmpty){
+    if (currentArtForms.isEmpty) {
       return Center(
         child: Column(
           children: [
-            const SizedBox(height: 40.0,),
+            const SizedBox(
+              height: 40.0,
+            ),
             const Text(
               'No art forms to display',
-              style: TextStyle(
-                color: Colors.teal,
-                fontSize: 30.0
-              ),
+              style: TextStyle(color: Colors.teal, fontSize: 30.0),
             ),
             SliderTheme(
               data: const SliderThemeData(
-                  showValueIndicator: ShowValueIndicator.onlyForDiscrete
-              ),
+                  showValueIndicator: ShowValueIndicator.onlyForDiscrete),
               child: RangeSlider(
                 values: yearRange,
                 min: 0,
                 max: 2000,
                 divisions: 100,
-                labels: RangeLabels(
-                    yearRange.start.round().toString(),
-                    yearRange.end.round().toString()
-                ),
-                onChanged: (RangeValues values){
+                labels: RangeLabels(yearRange.start.round().toString(),
+                    yearRange.end.round().toString()),
+                onChanged: (RangeValues values) {
                   setState(() {
                     changedDropdown = false;
                     yearRange = values;
@@ -93,14 +86,15 @@ class _MapPageState extends State<MapPage> {
             hint: const Text('Art Form'),
             value: widget.selectedArtForm['artName'],
             onChanged: (newVal) {
-              if(newVal != null){
+              if (newVal != null) {
                 setState(() {
                   changedDropdown = true;
-                  widget.selectedArtForm = widget.ArtForms.firstWhere((element) => element['artName'] == newVal);
+                  widget.selectedArtForm = widget.ArtForms.firstWhere(
+                      (element) => element['artName'] == newVal);
                 });
               }
             },
-            items: currentArtForms.map((af){
+            items: currentArtForms.map((af) {
               return DropdownMenuItem(
                 value: af['artName'],
                 child: Text('${af['artName']}'),
@@ -120,18 +114,15 @@ class _MapPageState extends State<MapPage> {
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   userAgentPackageName: 'com.example.app',
                 ),
-                MarkerLayer(
-                    markers: [
-                      Marker(
-                          point: LatLng(lat, lng),
-                          builder: (context) => Icon(
+                MarkerLayer(markers: [
+                  Marker(
+                      point: LatLng(lat, lng),
+                      builder: (context) => Icon(
                             Icons.pin_drop,
                             color: Colors.pinkAccent[400],
                             size: 30.0,
-                          )
-                      )
-                    ]
-                )
+                          ))
+                ])
               ],
             ),
           ),
@@ -139,23 +130,20 @@ class _MapPageState extends State<MapPage> {
           /** Year Slider **/
           SliderTheme(
             data: const SliderThemeData(
-              showValueIndicator: ShowValueIndicator.onlyForDiscrete
-            ),
+                showValueIndicator: ShowValueIndicator.onlyForDiscrete),
             child: RangeSlider(
               values: yearRange,
               min: 0,
               max: 2000,
               divisions: 100,
-              labels: RangeLabels(
-              yearRange.start.round().toString(),
-              yearRange.end.round().toString()
-              ),
-              onChanged: (RangeValues values){
-                  setState(() {
-                    changedDropdown = false;
-                    yearRange = values;
-                  });
-                },
+              labels: RangeLabels(yearRange.start.round().toString(),
+                  yearRange.end.round().toString()),
+              onChanged: (RangeValues values) {
+                setState(() {
+                  changedDropdown = false;
+                  yearRange = values;
+                });
+              },
             ),
           )
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -15,16 +15,16 @@ void main() async{
   Map<String, dynamic> selectedArtForm = {};
 
   /*** fetch art forms **/
-  try{
+  try {
     final artFormsRef = FirebaseFirestore.instance.collection('artForms');
     List<Map<String, dynamic>> allArtForms = [];
     await artFormsRef.get().then((snapshot) => {
-      snapshot.docs.forEach((element) {
-        allArtForms.add(element.data());
-      })
-    });
+          snapshot.docs.forEach((element) {
+            allArtForms.add(element.data());
+          })
+        });
     ArtForms = allArtForms;
-    if(ArtForms.isNotEmpty) selectedArtForm = ArtForms[0];
+    if (ArtForms.isNotEmpty) selectedArtForm = ArtForms[0];
   } catch (err) {
     print('Error loading documents: $err');
   }
@@ -45,9 +45,7 @@ void main() async{
         ),
       ),
       body: MapPage(ArtForms: ArtForms, selectedArtForm: selectedArtForm),
-      bottomNavigationBar: const Navbar(),
+      //bottomNavigationBar: Navbar(),
     ),
   ));
 }
-
-
