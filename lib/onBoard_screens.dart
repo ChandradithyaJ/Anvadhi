1bb2b75035fcf2940216bb24239440504a774bb6
Arpit Gupta <98796025+arpitguptagithub@users.noreply.github.com>
2023-08-22 01:27:13 +0530

    Update onBoard_screens.dart

--- lib/onBoard_screens.dart
+++ lib/onBoard_screens.dart
@@ -1,5 +1,10 @@
+import 'package:anvadhi/BoardingPage1.dart';
+import 'package:anvadhi/BoardingPage2.dart';
+import 'package:anvadhi/Home_screen_culturia.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:anvadhi/BoardingPage3.dart';
+
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
@@ -8,106 +13,120 @@ class OnBoardingScreen extends StatefulWidget {
   _OnBoardingScreenState createState() => _OnBoardingScreenState();
 }
 
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  
+class _OnBoardingScreenState extends State<OnBoardingScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
   PageController controller = PageController();
 
   int index = 0;
-  
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+  }
+
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    
       body: Stack(
-
-        children:[ PageView(
-          controller: controller,
-          onPageChanged: (index){
-            setState(() {
-              this.index = index;
-            });
-          },
-          children: [
-
-            // mannu replace containers to pages via routes 
-            Container(
-              color: Colors.red,
-              child: Text("OnBoardingScreen"),
-            ),
-             Container(
-              color: Colors.orange,
-              child: Text("OnBoardingScreen2"),
-            ),
+        children: [
+          PageView(
+            controller: controller,
+            onPageChanged: (index) {
+              setState(() {
+                this.index = index;
+              });
+            },
+            children: [
+              // mannu replace containers to pages via routes
               Container(
                 color: Colors.orangeAccent,
-                child: Text("OnBoardingScree3"),
+                child: BoardingPage1(),
               ),
-          ],
-        ),
-
-        Container(
-          alignment: Alignment.bottomLeft,
-
-         
-
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [ 
-
-              GestureDetector(
-                onTap: (){
-                  controller.jumpToPage(2);
-                },
-               
-          
-                  child: Center(child: Text("Skip")),
-              
+              Container(
+                color: Colors.orange,
+                child: BoardingPage2(),
               ),
-            
-              SmoothPageIndicator(
-              controller: controller,
-              count: 3,
-              effect: ExpandingDotsEffect(
-                activeDotColor: Colors.orange,
-                dotColor: Colors.grey,
-                dotHeight: 4,
-                dotWidth: 6,
-                spacing: 5,
-                  ),),
-
-            index != 2 ?
-              GestureDetector(
-                onTap: (){
-                  controller.nextPage( duration: Duration(milliseconds: 600), curve: Curves.easeInOut);
-                },
-               
-               
-                  child: Center(child: Text("Next")),
-              )
-                
-                : GestureDetector(
-                onTap: (){
-                  controller.nextPage(duration: Duration(milliseconds:600 ), curve: Curves.easeInOut);
-
-                }, 
-
-                child: Container(
-                  height: 50,
-                  width: 100,
-                  color: Colors.blue,
-                  child: Center(child: Text("Get Started")),
-                ),
-                ),
-              
+              Container(
+                child: BoardingPage3(),
+              ),
             ],
           ),
-        ),
-      
-        ],
-        ),
-    );
-     
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.all(90.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          controller.jumpToPage(2);
+                        },
+                        child: Center(child: Text("Skip")),
+                      ),
+                      SmoothPageIndicator(
+                        controller: controller,
+                        count: 3,
+                        effect: ExpandingDotsEffect(
+                          activeDotColor: Colors.orange,
+                          dotColor: Colors.grey,
+                          dotHeight: 4,
+                          dotWidth: 6,
+                          spacing: 5,
+                        ),
+                      ),
+                      index != 2
+                          ? GestureDetector(
+                              onTap: () {
+                                controller.nextPage(
+                                    duration: Duration(milliseconds: 600),
+                                    curve: Curves.easeInOut);
+                              },
+                              child: Center(child: Text("Next",style: TextStyle( fontSize: 10),) ,),
+                            )
+                          : GestureDetector(
+                              onTap: () {
+                                controller.nextPage(
+                                    duration: Duration(milliseconds: 600),
+                                    curve: Curves.easeInOut);
+                              },
+                            child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              Home_screen_culturia()));
+                                    
 
-    
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.purple[100]
+                                ),
+                                
+                                child: Text("Get Started")),
+                                
+                            )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
