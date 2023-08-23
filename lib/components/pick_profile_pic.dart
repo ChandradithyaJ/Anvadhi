a26ce5fb53ad65c1f7affea83ee56102fcf0d445
Chandradithya <chandradithya@gmail.com>
2023-08-24 01:08:10 +0530

    UI change in pick profile pic page

--- lib/components/pick_profile_pic.dart
+++ lib/components/pick_profile_pic.dart
@@ -20,13 +20,13 @@ class _PickProfilePicState extends State<PickProfilePic> {
   @override
   Widget build(BuildContext context) {
     List<String> profilepics = [
-      'https://i.pinimg.com/474x/fd/c4/ea/fdc4ea89768a65c947634967b72fe845.jpg',
       'https://newevolutiondesigns.com/images/wallpapers/cool-4k-wallpaper-9.jpg',
       'https://imgs.search.brave.com/otbddk0Y3dateXqMWzum8T3biCXfqGE45FyxpZb2Xms/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9iZXN0/cHJvZmlsZXBpY3R1/cmVzLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMS8wNC9D/b29sLVBpYy03Nzl4/MTAyNC5qcGc',
       'https://imgs.search.brave.com/_XBoT6A6-hJHfQiiQcQMa_Yo3p3zNG8D2i4isjD3rd4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2E3Lzk2/L2U5L2E3OTZlOWIx/ZmRkNTE5NDg5NTk5/Y2I3YTIzOGZhNmJj/LmpwZw',
       'https://e1.pxfuel.com/desktop-wallpaper/497/182/desktop-wallpaper-city-anime-girl-alone-anime-alone-girl-thumbnail.jpg',
       'https://e1.pxfuel.com/desktop-wallpaper/207/1021/desktop-wallpaper-yourname-night-anime-sky-illustration-art-android-anime-sky-thumbnail.jpg',
       'https://e0.pxfuel.com/wallpapers/520/288/desktop-wallpaper-samurai-sky-art-thumbnail.jpg',
+      'https://i.pinimg.com/474x/fd/c4/ea/fdc4ea89768a65c947634967b72fe845.jpg',
       'https://i.pinimg.com/236x/2f/5d/ce/2f5dce84606a38db3845236528ed3571.jpg',
       'https://i.pinimg.com/236x/ff/2e/a2/ff2ea2726810dd34b711936664becf50.jpg'
     ];
@@ -60,7 +60,7 @@ class _PickProfilePicState extends State<PickProfilePic> {
                     return Row(
                       children: <Widget>[
                         Expanded(
-                          child: ClipOval(
+                          child: ClipRect(
                             child: Material(
                               color: Colors.transparent,
                               child: Ink.image(
